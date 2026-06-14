2c824604c523aa1c832be7538c20a0d7bc7c23b6
makbarzidane <168658565+makbarzidane@users.noreply.github.com>
2026-06-14 17:01:14 +0700

    feat(types): add next-auth module augmentation for Role

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,19 @@
+import { Role } from './index'
+import { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    role: Role
+  }
+  interface Session {
+    user: {
+      role: Role
+    } & DefaultSession['user']
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role: Role
+  }
+}
